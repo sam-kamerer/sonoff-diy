221d886cdf83d5e60b850628997802a75593aac5
Andrii Semerun <andrii.semerun@gmail.com>
2020-12-10 09:04:18 +0200

    Fix printing of information about discovered devices

--- pkg/cmd/discover.go
+++ pkg/cmd/discover.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/brutella/dnssd"
 	"github.com/urfave/cli/v2"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -25,27 +26,30 @@ type (
 
 const service = "_ewelink._tcp.local."
 
+func addServiceFunc(w io.Writer) dnssd.AddServiceFunc {
+	return func(srv dnssd.Service) {
+		data := Data{}
+		if err := json.Unmarshal([]byte(strings.ReplaceAll(srv.Text["data1"], "\\", "")), &data); err != nil {
+			log.Printf("could not parse data1: %v", err)
+		}
+
+		_, _ = fmt.Fprintf(w, "%s:%d\t%s.%s\t%s\t%d dB\n",
+			srv.IPs[0].String(), srv.Port, srv.Host, srv.Domain, srv.Text["type"], data.RSSI)
+	}
+}
+
 func List(c *cli.Context) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(c.Int("duration")))
 	defer cancel()
 
-	addFn := func(srv dnssd.Service) {
-		data := &Data{}
-		if err := json.Unmarshal([]byte(strings.ReplaceAll(srv.Text["data1"], "\\", "")), data); err != nil {
-			log.Printf("could not parse data1: %v", err)
-		}
+	w := tabwriter.NewWriter(os.Stdout, 0, 8, 4, '\t', 0)
+	_, _ = fmt.Fprintf(w, "IP:Port\tHost\tType\tSignal\n")
+	_, _ = fmt.Fprintf(w, "-------\t----\t----\t------\n")
 
-		w := tabwriter.NewWriter(os.Stdout, 0, 8, 4, '\t', 0)
-		_, _ = fmt.Fprintf(w, "IP:PORT\tHost\tType\tSignal\n")
-		_, _ = fmt.Fprintf(w, "-------\t----\t----\t------\n")
-		_, _ = fmt.Fprintf(w, "%s:%d\t%s.%s\t%s\t%d dB\n",
-			srv.IPs[0].String(), srv.Port, srv.Host, srv.Domain, srv.Text["type"], data.RSSI)
-		_ = w.Flush()
-	}
-
-	err := dnssd.LookupType(ctx, service, addFn, nil)
+	err := dnssd.LookupType(ctx, service, addServiceFunc(w), nil)
 	if err == context.DeadlineExceeded {
 		err = nil
 	}
+	_ = w.Flush()
 	return err
 }
