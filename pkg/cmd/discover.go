d44b356d2bbd299db78bc8686b7837d7d6ec673c
Andrey Semerun <andrey.semerun@gmail.com>
2021-12-21 16:23:39 +0200

    Replace package 'github.com/brutella/dnssd' by 'github.com/grandcat/zeroconf'

--- pkg/cmd/discover.go
+++ pkg/cmd/discover.go
@@ -4,14 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/brutella/dnssd"
-	"github.com/urfave/cli/v2"
 	"io"
 	"log"
+	"net/url"
 	"os"
 	"strings"
 	"text/tabwriter"
 	"time"
+
+	"github.com/grandcat/zeroconf"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
 )
 
 type (
@@ -24,32 +27,71 @@ type (
 	}
 )
 
-const service = "_ewelink._tcp.local."
-
-func addServiceFunc(w io.Writer) dnssd.AddServiceFunc {
-	return func(srv dnssd.Service) {
-		data := Data{}
-		if err := json.Unmarshal([]byte(strings.ReplaceAll(srv.Text["data1"], "\\", "")), &data); err != nil {
-			log.Printf("could not parse data1: %v", err)
-		}
-
-		_, _ = fmt.Fprintf(w, "%s:%d\t%s.%s\t%s\t%d dB\n",
-			srv.IPs[0].String(), srv.Port, srv.Host, srv.Domain, srv.Text["type"], data.RSSI)
-	}
-}
+const (
+	serviceType   = "_ewelink._tcp"
+	serviceDomain = "local."
+)
 
 func List(c *cli.Context) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(c.Int("duration")))
-	defer cancel()
+	service := c.String("serviceType")
+	if service == "" {
+		service = serviceType
+	}
+	domain := c.String("serviceDomain")
+	if domain == "" {
+		domain = serviceDomain
+	}
+
+	// Discover all services on the network (e.g. _workstation._tcp)
+	resolver, err := zeroconf.NewResolver(nil)
+	if err != nil {
+		return errors.Wrap(err, "failed to initialize resolver")
+	}
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 8, 4, '\t', 0)
-	_, _ = fmt.Fprintf(w, "IP:Port\tHost\tType\tSignal\n")
-	_, _ = fmt.Fprintf(w, "-------\t----\t----\t------\n")
+	_, _ = fmt.Fprintf(w, "IP\tPort\tHost\tType\tSignal\n")
+	_, _ = fmt.Fprintf(w, "--\t----\t----\t----\t------\n")
 
-	err := dnssd.LookupType(ctx, service, addServiceFunc(w), nil)
-	if err == context.DeadlineExceeded {
-		err = nil
+	entries := make(chan *zeroconf.ServiceEntry)
+	go func(results <-chan *zeroconf.ServiceEntry) {
+		for entry := range results {
+			printEntry(w, entry)
+		}
+	}(entries)
+
+	fmt.Printf("Started services discovering...\n\n")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(c.Int("duration")))
+	defer cancel()
+	err = resolver.Browse(ctx, service, domain, entries)
+	if err != nil {
+		return errors.Wrap(err, "failed to browse services")
 	}
+	<-ctx.Done()
 	_ = w.Flush()
-	return err
+
+	return nil
+}
+
+func printEntry(w io.Writer, entry *zeroconf.ServiceEntry) {
+	dataValues, err := url.ParseQuery(strings.Join(entry.Text, "&"))
+	if err != nil {
+		log.Printf("could not parse entry text: %v", err)
+	}
+
+	data := Data{}
+	if err := json.Unmarshal([]byte(strings.ReplaceAll(dataValues.Get("data1"), "\\", "")), &data); err != nil {
+		log.Printf("could not parse data1: %v", err)
+	}
+
+	ips := make([]string, 0)
+	for _, ipV4 := range entry.AddrIPv4 {
+		ips = append(ips, ipV4.String())
+	}
+	if len(ips) == 0 {
+		for _, ipV6 := range entry.AddrIPv6 {
+			ips = append(ips, ipV6.String())
+		}
+	}
+	_, _ = fmt.Fprintf(w, "%s\t%d\t%s\t%s\t%d dB\n",
+		strings.Join(ips, ", "), entry.Port, entry.ServiceInstanceName(), dataValues.Get("type"), data.RSSI)
 }
